056e5e8442e74356e3c57807906403fab8f4f79c
devhirano <devhirano@gmail.com>
2017-01-13 09:27:47 +0900

    fix sublime3

--- ubuntu1604/sublime3-plugins.sh
+++ ubuntu1604/sublime3-plugins.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+sudo apt update 
+
+# for Japanese
+sudo apt -y install emacs-mozc
+
 # for markmon plugin
 sudo npm install -g markmon
-sudo apt update 
 sudo apt -y install pandoc
---
+++ ubuntu1604/sublime3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install package
+## https://www.sublimetext.com/3
+curl -O https://download.sublimetext.com/sublime-text_build-3126_amd64.deb
+sudo dpkg -i sublime-text_build-3126_amd64.deb
+
+# install package control plugin
+## https://packagecontrol.io/installation
+
