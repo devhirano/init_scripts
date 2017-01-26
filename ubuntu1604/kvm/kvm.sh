506de3a9084140abcac7fb6110434732bee4bc87
devhirano <devhirano@gmail.com>
2017-01-26 12:23:51 +0900

    add kvm pakcages for ex virt-install

--- ubuntu1604/kvm/kvm.sh
+++ ubuntu1604/kvm/kvm.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 
-sudo apt -y install qemu-kvm libvirt-bin virtinst bridge-utils
+sudo apt -y install qemu-kvm libvirt-bin virtinst bridge-utils libosinfo-bin libguestfs-tools virt-top
 
 sudo modprobe vhost_net
 sudo cat /etc/modules | grep vhost_net || sudo sh -c 'echo "vhost_net" >> /etc/modules'
