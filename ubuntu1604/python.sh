16428d63a7ad5e4f8959facda3cf333cb7eeb4e2
devhirano <devhirano@gmail.com>
2017-02-08 01:26:02 +0900

    add python

---
+++ ubuntu1604/python.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt install -y gcc g++ make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
+
+git clone -q https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone -q https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+grep PYENV_ROOT ~/.bashrc >/dev/null
+if [ $? != 0 ];then
+cat << 'EOT' >> ~/.bashrc
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+EOT
+fi
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+pyenv install -f 3.5.1
+pyenv rehash
+pyenv global 3.5.1
