24409d3ed03eb8eb0d9c22bc4146cf9d798632c2
devhirano <devhirano@gmail.com>
2017-02-09 01:56:30 +0900

    add kvm virt-manager

--- ubuntu1604/kvm/kvm.sh
+++ ubuntu1604/kvm/kvm.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 
-sudo apt -y install qemu-kvm libvirt-bin virtinst bridge-utils libosinfo-bin libguestfs-tools virt-top
+sudo apt -y install qemu-kvm libvirt-bin virtinst bridge-utils libosinfo-bin libguestfs-tools virt-top virt-manager
 
 sudo modprobe vhost_net
 sudo cat /etc/modules | grep vhost_net || sudo sh -c 'echo "vhost_net" >> /etc/modules'
