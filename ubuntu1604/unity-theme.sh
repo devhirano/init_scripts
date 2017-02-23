6c1ae646b4009acb91bb552f6cd5160efcb641f4
devhirano <devhirano@gmail.com>
2017-02-24 01:38:42 +0900

    add icons

--- ubuntu1604/unity-theme.sh
+++ ubuntu1604/unity-theme.sh
@@ -12,6 +12,31 @@ sudo mv Yosembiance-master/Yosembiance-* /usr/share/themes/
 # https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
 sudo wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install-papirus-root.sh | sh
 
+### ICONS ###
+# numix-icon-theme-circle
+sudo -E add-apt-repository -y ppa:numix/ppa
+
+# Faenza
+# sudo -E add-apt-repository -y ppa:tiheum/equinox
+
+# Awoken
+# sudo -E add-apt-repository -y ppa:alecive/antigone
+
+# vibrancy-colors
+sudo -E add-apt-repository -y ppa:ravefinity-project/ppa
+
+# evolvere-icon-suite
+# sudo -E add-apt-repository -y ppa:noobslab/evolvere
+
+
+sudo apt-get update
+
+
+sudo apt install -y \
+        numix-icon-theme-circle \
+        faenza-icon-theme \
+        vibrancy-colors
+
 
 popd
 
