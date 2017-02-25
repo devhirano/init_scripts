276d7aef13a451f370f865428c36ac50e601b4b1
devhirano <devhirano@gmail.com>
2017-02-26 00:33:44 +0900

    add golang

---
+++ ubuntu1604/golang.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# https://golang.org/doc/install
+
+pushd /tmp
+sudo wget https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz
+sudo tar xf go1.8.linux-amd64.tar.gz -C /usr/local/
+
+grep "/usr/local/go/bin" ~/.bashrc >/dev/null
+if [ $? != 0 ];then
+  mkdir -p ~/.go/{bin,pkg,src}
+  echo 'export GOPATH=$HOME/.go' >> ~/.bashrc
+  echo 'export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin' >> ~/.bashrc
+  echo '' >> ~/.bashrc
+fi
+source ~/.bashrc
+
+# check
+go version
+
+popd
+
+
+
