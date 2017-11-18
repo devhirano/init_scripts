048d6ca38c0c537ae0e0e45cca5b0d52f774d49f
devhirano <devhirano@gmail.com>
2017-11-18 16:51:37 +0900

    add unity theme arc, flatabulous with ultraflat icon

--- ubuntu1604/unity-theme.sh
+++ ubuntu1604/unity-theme.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 pushd /tmp
 
@@ -12,9 +12,25 @@ sudo mv Yosembiance-master/Yosembiance-* /usr/share/themes/
 # https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
 sudo wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install-papirus-root.sh | sh
 
+# ARC theme
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list" && \
+wget -q -O - http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key | sudo apt-key add - && \
+sudo apt update && sudo apt install arc-theme -y
+
+# Flatabulous
+# https://github.com/anmoljagetia/Flatabulous
+sudo add-apt-repository -y ppa:noobslab/themes && \
+sudo apt update && \
+sudo apt -y install flatabulous-theme
+
 ### ICONS ###
 # numix-icon-theme-circle
 sudo -E add-apt-repository -y ppa:numix/ppa
+
+# UltraFlat
+sudo add-apt-repository -y ppa:noobslab/icons && \
+sudo apt update && \
+sudo apt -y install ultra-flat-icons
 
 # Faenza
 # sudo -E add-apt-repository -y ppa:tiheum/equinox
