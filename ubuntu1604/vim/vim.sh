e7c568daf39442d1458483d3c7971dac7611ec05
devhirano <devhirano@gmail.com>
2018-02-01 23:40:44 +0900

    replace YCM to jedi-vim with supertab

--- ubuntu1604/vim/vim.sh
+++ ubuntu1604/vim/vim.sh
@@ -1,4 +1,47 @@
-#!/bin/bash -xe
+#!/bin/bash -e
+
+howtoreinstallvim () {
+cat << EOT
+
+---------------------
+$ sudo apt purge vim vim-runtime vim-common
+$ sudo install -y git build-essential ncurses-dev lua5.2 lua5.2-dev luajit python-dev python3-dev ruby-dev
+$ TEMP=$(mktemp -d)
+$ git clone https://github.com/vim/vim $TEMP/vim
+$ pushd $TEMP/vim
+$ sudo ./configure --with-features=huge --enable-multibyte \
+--enable-luainterp=dynamic --enable-gpm \
+--enable-cscope --enable-fontset --enable-fail-if-missing \
+--prefix=/usr/local --enable-pythoninterp=dynamic \
+--enable-python3interp=dynamic --enable-rubyinterp=dynamic 
+$ sudo make
+$ sudo make install
+---------------------
+
+EOT
+}
+
+if [ "$1" != "-f" ];then
+    vim --version |grep -e "+python/dyn" >/dev/null 2>&1
+    if [ $? -ne 0 ];then
+        echo "Python dynamic link library is disabled, so you would have some limitations."
+        echo "Set '-f' to install if you don't care."
+        echo
+        echo "Or, you MAYBE can re-install vim as follows."
+        howtoreinstallvim
+        exit 1
+    fi
+
+    vim --version |grep -e "+lua/dyn" >/dev/null 2>&1
+    if [ $? -ne 0 ];then
+        echo "Lua dynamic link library is disabled, so you would have some limitations."
+        echo "Set '-f' to install if you don't care."
+        echo
+        echo "Or, you MAYBE can re-install vim as follows."
+        howtoreinstallvim
+        exit 1
+    fi
+fi
 
 sudo apt update
 sudo apt install -y vim vim-gnome build-essential cmake python-dev python3-dev exuberant-ctags npm
@@ -8,11 +51,13 @@ pip install flake8
 sudo npm -g install coffeelint
 
 # install neobundle
-pushd /tmp
+TEMPDIR=$(mktemp -d)
+pushd $TEMPDIR
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 chmod +x ./install.sh
 sh ./install.sh
 popd
+rm -rf $TEMPDIR
 
 mkdir -p ~/.vim/colors
 
