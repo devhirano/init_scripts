bdae14f7305e8f3ddb1506c81ed75c6515d86884
devhirano <devhirano@gmail.com>
2018-02-15 23:21:29 +0900

    add pidgin and sipe for skype for business

--- ubuntu1604/skype.sh
+++ ubuntu1604/skype.sh
@@ -2,6 +2,9 @@
 
 sudo sed -i.bak "/^# deb .*partner/ s/^# //" /etc/apt/sources.list 
 sudo apt update
+
+# Skype
 sudo apt install -y skype pulseaudio
 
-
+# Skype for business
+sudo apt install -y pidgin pidgin-sipe
