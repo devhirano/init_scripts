5c865318ea8e835a0e875322d21880f6d75aa419
devhirano <devhirano@gmail.com>
2018-05-17 15:54:43 +0900

    add alias for git

--- ubuntu1604/git/git.sh
+++ ubuntu1604/git/git.sh
@@ -9,3 +9,24 @@ git config --global user.useConfigOnly true
 # ignore global file
 git config --global core.excludesfile ~/.gitignore_global
 cp gitignore_global ~/.gitignore_global
+
+grep alias ~/.gitconfig 2>&1 >/dev/null
+if [ $? -ne 0 ];then
+cat << EOT >> ~/.gitconfig
+[alias]
+    gr = log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'
+    l = log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'
+    s = status
+    cm = commit
+    co = checkout
+    su = status -uno
+    difff = diff --word-diff
+EOT
+fi
+
+grep 'alias g=' ~/.alias 2>&1 >/dev/null
+cat << EOT >> ~/.alias
+alias g='git'
+alias gs='git s'
+alias gl='git l'
+EOT
