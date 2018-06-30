6a7a6a906b646f245c191f3415293e60f42a2397
devhirano <devhirano@gmail.com>
2018-06-30 23:52:28 +0900

    add winehq

--- ubuntu1604/wine.sh
+++ ubuntu1604/wine.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo add-apt-repository -y ppa:noobslab/apps
+sudo add-apt-repository -y ppa:wine/wine-builds
 sudo apt update
+sudo apt -y install --install-recommends winehq-staging
 sudo apt install -y playonlinux
 
