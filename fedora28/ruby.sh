5ca6f1e501b927e8581674d5448dbff729a11ee8
devhirano <devhirano@gmail.com>
2018-09-14 23:30:21 +0900

    fix ruby

--- fedora28/ruby.sh
+++ fedora28/ruby.sh
@@ -2,15 +2,17 @@
 
 # https://developer.fedoraproject.org/start/sw/web-app/rails.html
 dnf -y install git-core zlib zlib-devel gcc-c++ patch readline readline-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison curl sqlite-devel \
-               gcc-6 gdbm-devel ncurses-devel
+               gdbm-devel ncurses-devel
 
-rvm implode
+which rvm && rvm implode
 
-git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+[ -z "~/.rbenv" ] || git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-# exec $SHELL
-source ~/.bashrc
+
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
 
 mkdir -p ~/.rbenv/plugins
 cd ~/.rbenv/plugins
