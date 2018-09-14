615ed3d9e66b59185aa148bd08c1bf01b1985569
devhirano <devhirano@gmail.com>
2018-09-14 23:31:37 +0900

    add vim master

--- fedora28/vim/install-master.sh
+++ fedora28/vim/install-master.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-dnf -y install ncurses ncurses-dev lua-devel && \
+set -ex
+
+which vim || \
+    sudo dnf -y install ncurses ncurses-devel lua-devel && \
     TEMP=$(mktemp -d) && \
     git clone https://github.com/vim/vim $TEMP/vim && \
     pushd $TEMP/vim && \
@@ -10,4 +13,46 @@ dnf -y install ncurses ncurses-dev lua-devel && \
     --prefix=/usr/local \
     --enable-python3interp=dynamic --enable-rubyinterp=dynamic && \
     make && \
-    make install
+    sudo make install && \
+    popd
+
+# install syntastic libs
+pip install flake8
+mv ~/.flake8 ~/.flake8.bak >/dev/null 2>&1 && \
+    echo "~/.flake8 is backed up to ~/.flake8.bak"
+cp flake8 ~/.flake8
+
+pip install jedi
+npm -g install coffeelint
+
+# install vim-autoformat
+pip install autopep8 yapf
+
+# install neobundle
+TEMPDIR=$(mktemp -d)
+pushd $TEMPDIR
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+chmod +x ./install.sh
+sh ./install.sh
+popd
+rm -rf $TEMPDIR
+
+mkdir -p ~/.vim/colors
+
+# colors
+grep "Hybrid Terminal Colours." ~/.Xresources 2>/dev/null 1>/dev/null || cat Xresources >> ~/.Xresources
+curl -o ~/.vim/colors/hybrid.vim https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim
+
+pwd
+cp vimrc ~/.vimrc
+
+# install plugins
+vim +NeoBundleInstall +qall
+
+# disable install YCM
+# pushd ~/.vim/bundle/YouCompleteMe/
+# ./install.py --clang-completer
+# popd
+
+# TAGLIST
+cp -pR taglist/* ~/.vim/
