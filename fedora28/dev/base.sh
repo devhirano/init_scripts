b4fadf7a5dcbdd49fce5240b785e2768043db27d
devhirano <devhirano@gmail.com>
2018-09-30 00:30:50 +0900

    add dev tools

--- fedora28/dev/base.sh
+++ fedora28/dev/base.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-dnf groupinstall -y "Development Tools" "Development Libraries"
+dnf groupinstall -y "Development Tools" "Development Libraries" buildsys-build
 dnf -y install kernel-devel-`uname -r`
-dnf -y install libuuid libuuid-devel
-dnf -y install nasm
-dnf -y install flex
+dnf -y install libuuid libuuid-devel nasm flex bison texinfo
