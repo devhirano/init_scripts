af7c882064af21333b32539a6fca3ee461afeb98
devhirano <devhirano@gmail.com>
2018-10-10 23:22:36 +0900

    add devtools for fedora

--- fedora28/dev/base.sh
+++ fedora28/dev/base.sh
@@ -3,3 +3,4 @@
 dnf groupinstall -y "Development Tools" "Development Libraries" buildsys-build
 dnf -y install kernel-devel-`uname -r`
 dnf -y install libuuid libuuid-devel nasm flex bison texinfo
+dnf -y install gmp  gmp-devel  mpfr  mpfr-devel  libmpc  libmpc-devel
