1f0aeec4366a22f7c9bf47d1b5ee90b2c076e071
devhirano <devhirano@gmail.com>
2019-07-20 12:36:52 +0900

    add docker-compose

--- fedora28/docker.sh
+++ fedora28/docker.sh
@@ -12,3 +12,8 @@ sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/dock
 sudo dnf -y install docker-ce docker-ce-cli containerd.io
 
 sudo systemctl enable docker
+
+# docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
