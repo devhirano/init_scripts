fe270b1446ef44b19576b43fe2bb52a3e73edc0a
devhirano <devhirano@gmail.com>
2019-10-31 12:31:46 +0900

    add screen buffer check command

--- ubuntu1604/screen/bin/clipboard_exchange.sh
+++ ubuntu1604/screen/bin/clipboard_exchange.sh
@@ -3,9 +3,26 @@
 ACTION=$1
 
 if [ "$ACTION" = "screen-to-x" ];then
-  screen -X writebuf && xsel -bi < $HOME/.screen-exchange && screen -X echo "copy to clipboard!"
+    screen -X writebuf && \
+        sync && \
+        xsel -bi < $HOME/.screen-exchange && \
+        screen -X echo "copy to clipboard! : $(xsel -bo | head -n 1)"
 
 elif [ "$ACTION" = "x-to-screen" ];then
-  xsel -bo > $HOME/.screen-exchange && screen -X readbuf && screen -X echo "copy to buffer!"
+    sync && \
+        xsel -bo > $HOME/.screen-exchange && \
+        screen -X readbuf && \
+        screen -X echo "copy to buffer! : $(head -n 1 $HOME/.screen-exchange)"
 
+elif [ "$ACTION" = "check-buffer" ];then
+    screen -X bufferfile $HOME/.screen-exchange-tmp
+    echo
+    echo "  #### [ screen copy/paste buffer | head -n 10 ]"
+    screen -X writebuf && sync
+    cat $HOME/.screen-exchange-tmp | head -n 10 | sed -e "s/^/  #### /g"
+    echo
+    echo "  #### [ X clipboard buffer | head -n 10 ]"
+    xsel -bo | head -n 10 | sed -e "s/^/  #### /g"
+    echo
+    screen -X bufferfile $HOME/.screen-exchange
 fi
